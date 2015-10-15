5b78643a8cdfb2b2ac31b0fcc813d5980b23fdf2
Mimi Schatz <mcschatz@gmail.com>
2015-10-15 10:17:21 -0600

    test added for store model

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,13 @@
 class Store < ActiveRecord::Base
+  belongs_to :user
+  has_many :loans
+
+  validates :name, uniqueness: true, presence: true
+  validates :user_id, presence: true
+
+  before_validation :capitalize
+
+  def capitalize
+    self.name = name.to_s.capitalize
+  end
 end
--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Store, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is valid" do
+    user  = User.create(username: "alice",
+            password: "password",
+            full_name: "Alice Jones",
+            address: "1500 Blake St., Denver, CO 80205")
+    store = Store.create(name: "Adam", user_id: user.id)
+    expect(store).to be_valid
+  end
+
+  it "is invalid if store name already exists" do
+    user =  User.create(username: "alice",
+            password: "password",
+            full_name: "Alice Jones",
+            address: "1500 Blake St., Denver, CO 80205")
+    store1 = Store.create(name: "Mimi", user_id: user.id)
+    store2 = Store.create(name: "mimi", user_id: user.id)
+
+    expect(store1).to be_valid
+    expect(store2).to_not be_valid
+  end
+
+  it "must belong to a user" do
+    user =  User.create(username: "alice",
+            password: "password",
+            full_name: "Alice Jones",
+            address: "1500 Blake St., Denver, CO 80205")
+    store = Store.create(name: "Mimi", user_id: user.id)
+    store.user_id = nil
+    expect(store).to_not be_valid
+  end
 end
