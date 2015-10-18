9a3103133edc7f423f4d94895456107234ba4970
MB Burch <marybethburch@gmail.com>
2015-10-18 15:28:28 -0600

    all users edit through same view and controller now. deleted seller and admin separate edit views.

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -5,24 +5,4 @@ class Admin::AdminsController < Admin::BaseController
     @cancelled = Order.where(status: "Cancelled")
     @ordered = Order.where(status: "Ordered")
   end
-
-  def edit
-    @admin = current_user
-  end
-
-  def update
-    admin = User.find(current_user.id)
-    if admin.update(admin_params)
-      redirect_to admin_dashboard_path
-    else
-      flash[:notice] = admin.errors.full_messages.join(", ")
-      redirect_to admin_edit_path
-    end
-  end
-
-  private
-
-  def admin_params
-    params.require(:admin).permit(:username, :password, :full_name, :address, :email)
-  end
 end
