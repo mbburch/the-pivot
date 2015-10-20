49106aafb93201bc775c27b0b6a98a03634b7582
MB Burch <marybethburch@gmail.com>
2015-10-20 13:33:42 -0600

    fix tests. failures or skips are related to carts and orders. Fixed tiny item. Was linking to admin

--- spec/features/visitor_can_view_items_spec.rb
+++ spec/features/visitor_can_view_items_spec.rb
@@ -1,17 +1,25 @@
 require "rails_helper"
 
-feature "visitor viewing items" do
+feature "visitor viewing auctions" do
   include_context("features")
 
-  scenario "can view items" do
-    visit "/items"
+  scenario "can view auctions" do
+    item = Item.create!(title: "First Item",
+                       description: "test description",
+                       category: category)
+    item2 = Item.create!(title: "Second Item",
+                        description: "other test description",
+                        category: other_category)
+    Auction.create!(starting_price: 20, item_id: item.id)
+    Auction.create!(starting_price: 15, item_id: item2.id)
+    visit "/auctions"
     within(".nav-wrapper") do
-      expect(page).to have_content("Items")
+      expect(page).to have_content("Auctions")
     end
     within(".items") do
-      expect(page).to have_content("test title")
-      expect(page).to have_content("test description")
-      expect(page).to have_content("$50.00")
+      expect(page).to have_content("First Item")
+      expect(page).to have_content("Second Item")
+      expect(page).to have_content("Current Bid")
     end
   end
 end
