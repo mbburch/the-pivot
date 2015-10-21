7e8e717b0a6fde1b53f3acafe3ce8980d6d6fc89
MB Burch <marybethburch@gmail.com>
2015-10-21 09:49:47 -0600

    same four cart tests not working. all others pass. updated images in seed data.

--- spec/features/guest_can_visit_a_store_show_page_spec.rb
+++ spec/features/guest_can_visit_a_store_show_page_spec.rb
@@ -8,17 +8,18 @@ feature "user can visit a store" do
                         address: "1500 Blake St., Denver, CO 80205",
                         email: "alice@example.com")
     store = Store.create(title: "Adam", description: "We sell everything", user_id: user.id)
-            Item.create(title: "Bike Bus",
+    item  = Item.create(title: "Bike Bus",
             description: "We'll bring our shop to you.",
             avatar: open("https://s3.amazonaws.com/mb-the-pivot/Hilltop_Bicycles_Summit_NJ.JPG"),
             category: Category.create(name: "Transportation"),
             store_id: store.id)
+    Auction.create(item_id: item.id, starting_price: 15)
 
     visit stores_path
     expect(page).to have_content("Adam")
     expect(page).to have_content("We sell everything")
     click_on "Adam"
-    expect(current_path).to eq(store_items_path(store.slug))
+    expect(current_path).to eq(store_auctions_path(store.slug))
     expect(page).to have_content("We'll bring our shop to you.")
   end
 end
