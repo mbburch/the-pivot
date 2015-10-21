de9c5a465f5cf0889db28b7275657820446fbb6f
Mimi Schatz <mcschatz@gmail.com>
2015-10-21 16:48:46 -0600

    fixed auction test and admin dashboard test

--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -5,67 +5,67 @@ RSpec.describe Auction, type: :model do
 
 
  it 'counting' do
-   expect(Auction.all.count).to eq(3)
+   expect(Auction.all.count).to eq(5)
  end
 
   it "is valid" do
-    expect(auction).to be_valid
+    expect(auction_one).to be_valid
   end
 
   it "is invalid with a negative price" do
-    auction.starting_price = -10
-    expect(auction).to_not be_valid
+    auction_one.starting_price = -10
+    expect(auction_one).to_not be_valid
   end
 
   it "is invalid with no amount" do
-    auction.starting_price = nil
-    expect(auction).to_not be_valid
+    auction_one.starting_price = nil
+    expect(auction_one).to_not be_valid
   end
 
   it "is invalid with no item" do
-    auction.item_id = nil
-    expect(auction).to_not be_valid
+    auction_one.item_id = nil
+    expect(auction_one).to_not be_valid
   end
 
   it "has status of open" do
-    expect(Auction.open.first.id).to eq(auction.id)
+    expect(Auction.open.first.id).to eq(auction_one.id)
     expect(Auction.open.count).to eq(1)
   end
 
-  it "it knows auction status" do
-    expect(auction.status).to eq("open")
+  it "it knows auction_one status" do
+    expect(auction_one.status).to eq("open")
     expect(auction_two.status).to eq("scheduled")
     expect(ended_auction.status).to eq("ended")
   end
 
   it "can be ended" do
-    expect(auction.status).to eq("open")
-    auction.ending_time = Time.now - 1.minute
-    expect(auction.status).to eq("ended")
+    expect(auction_one.status).to eq("open")
+    auction_one.ending_time = Time.now - 1.minute
+    expect(auction_one.status).to eq("ended")
   end
 
   it "knows the final bid amount when ended" do
-    auction.bids << bid
-    expect(bid.status).to eq("Winning Bid")
+    auction_one.bids << low_bid
+    expect(low_bid.status).to eq("Winning Bid")
 
-    auction.bids << higher_bid
+    auction_one.bids << higher_bid
     expect(higher_bid.status).to eq("Winning Bid")
 
-    auction.ending_time = Time.now - 1.minute
-    expect(auction.status).to eq("ended")
+    auction_one.ending_time = Time.now - 1.minute
+    expect(auction_one.status).to eq("ended")
 
-    expect(auction.highest_bid).to eq(201)
+    expect(auction_one.highest_bid).to eq(201)
   end
 
-  it "knows the winning user of the auction" do
-    expect(auction.status).to eq("open")
-    auction.bids << bid
-    auction.bids << higher_bid
-    auction.bids << winning_bid
+  it "knows the winning user of the auction_one" do
+    expect(auction_one.status).to eq("open")
+    auction_one.bids << bid
+    auction_one.bids << higher_bid
+    auction_one.bids << winning_bid
 
-    auction.ending_time = Time.now - 1.minute
-    expect(auction.status).to eq("ended")
-    expect(auction.highest_bid).to eq(400)
-    expect(auction.winner).to eq(user_two)
+    auction_one.ending_time = Time.now - 1.minute
+    expect(auction_one.status).to eq("ended")
+    expect(auction_one.highest_bid).to eq(400)
+    expect(auction_one.winner).to eq(user_two)
   end
 end
