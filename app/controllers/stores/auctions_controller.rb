bfe4032610a3688631bdf23c171281109a362d01
Adam Ki Jensen <adajensen@gmail.com>
2015-10-22 10:22:18 -0600

    commit to go to master

--- app/controllers/stores/auctions_controller.rb
+++ app/controllers/stores/auctions_controller.rb
@@ -3,10 +3,4 @@ class Stores::AuctionsController < ApplicationController
   def index
     @auctions = Store.find_by(slug: params[:store]).items.flat_map(&:auctions)
   end
-
-  private
-
-    def auction_params
-      params.require(:auction).permit(:starting_price, :item_id)
-    end
 end
