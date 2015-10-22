86d93b9dbb99004765deabe525aaa93e1162e003
MB Burch <marybethburch@gmail.com>
2015-10-21 22:11:15 -0600

    add test for new auction

--- app/controllers/seller/auctions_controller.rb
+++ app/controllers/seller/auctions_controller.rb
@@ -10,14 +10,13 @@ class Seller::AuctionsController < Seller::SellersController
   end
 
   def create
-    byebug
     auction = Auction.create(auction_params.except(:store_id))
     if auction.save
       flash[:notice] = "Your auction is all set!"
     else
       flash[:notice] = "Please make sure you have filled in all of the fields."
     end
-    redirect_to :back
+    # redirect_to :back
   end
 
   private
